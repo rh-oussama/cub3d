5277f38c23bd4d19674e07047bb563ae44dcc349
alamaoui <150472243+alamaoui@users.noreply.github.com>
2024-11-18 06:32:18 +0100

    stack buffer overflow problem fixed

--- bonus/game_sprite_bonus.c
+++ bonus/game_sprite_bonus.c
@@ -6,7 +6,7 @@
 /*   By: alamaoui <alamaoui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 03:25:33 by alamaoui          #+#    #+#             */
-/*   Updated: 2024/11/17 11:46:53 by alamaoui         ###   ########.fr       */
+/*   Updated: 2024/11/18 06:27:15 by alamaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,9 +80,20 @@ int	frame_draw(t_data *game, t_texture *sprite, int frame)
 int	sprite(t_data *data, t_texture *sprite, int frames)
 {
 	double			delta_time;
-	static int		frame = 0;
+	static int		frames_array[4] = {0, 0, 0, 0};
+	int				frame_index;
+	int				frame;
 	static double	frame_timer = 0.00;
 
+	if (sprite == data->first)
+		frame_index = 0;
+	else if (sprite == data->second)
+		frame_index = 1;
+	else if (sprite == data->third)
+		frame_index = 2;
+	else if (sprite == data->fourth)
+		frame_index = 3;
+	frame = frames_array[frame_index];
 	delta_time = 1.0 / 60.0;
 	frame_timer += delta_time;
 	if (frame_timer >= 0.06 || frame == 0)
@@ -90,6 +101,7 @@ int	sprite(t_data *data, t_texture *sprite, int frames)
 		frame = (frame + 1) % frames;
 		frame_timer = 0.0;
 	}
+	frames_array[frame_index] = frame;
 	frame_draw(data, sprite, frame);
 	return (frame);
 }
